5c27293d4f1a635ddf74cd10adb456036fb47f9e
Сергей <sergeym83@bk.ru>
2025-11-30 13:32:15 +0300

    homework while do while

--- src/Main.java
+++ src/Main.java
@@ -4,65 +4,95 @@ public class Main {
     public static void main(String[] args) {
         //задача 1;
         System.out.println("задача 1");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        int firstFriday = 7;
+        for (int i = firstFriday; i <= 31; i = i + 7) {
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет");
         }
 
-        //задача 2;
-        System.out.println("задача 2");
-        for (int i = 10; i >= 1; i--) {
-            System.out.println(i);
+        //задача 2 do-while method
+        System.out.println("задача 2 вариант do-while ");
+        int distance = 0;
+        // int z = 0; // message counter
+        do {
+            // z++;
+            System.out.println("Держитесь! Осталось " + (42195 - distance) + " метров");
+            distance = distance + 500;
+        } while (distance < 42195);
+
+        //задача 2 for method
+        System.out.println("задача 2 вариант for");
+        distance = 0;
+        //z = 0; // message counter
+        for (; distance < 42195; distance = distance + 500) {
+            //   z++;
+            System.out.println("Держитесь! Осталось " + (42195 - distance) + " метров");
         }
-        //задача 3;
-        System.out.println("задача 3");
-        for (int i = 0; i < 17; i = i + 2) {
-            System.out.println(i);
+
+        //задача 3 while method
+        System.out.println("задача 3 вариант while ");
+        int money = 1500;
+        int day = 0;
+        while (money >= 100) {
+            day++;
+            if (day >= 5 && day % 5 == 0) {
+                continue;
+            }
+            money = money - 100;
         }
+        System.out.println("вы можете оставить свой автомобиль на количество дней равное " + day);
+
+        //задача 3 for method
+        System.out.println("задача 3 вариант for");
+        money = 900;
+        day = 0;
+        for (; money >= 100; day++) {
+            if (day >= 5 && day % 5 == 0) {
+                continue;
+            }
+            money = money - 100;
+        }
+        System.out.println("вы можете оставить свой автомобиль на количество дней равное " + day);
 
         //задача 4
         System.out.println("задача 4");
-        for (int i = 10; i >= -10; i--) {
-            System.out.println(i);
+        double total = 0;
+        int month = 0;
+        while (true) {
+            month++;
+            total = total + 15000;
+            if (month >= 6 && month % 6 == 0) {
+                total = total * 1.07;
+            }
+            if (total >= 12_000_000) {
+                break;
+            }
+            System.out.println("на " + month + " месяц, сумма накоплений составила: " + total);
         }
+        System.out.println("вы накопите 12_000_000 за " + month + " месяцев");
+        System.out.println("сумма накоплений составила: ");
+        System.out.printf("%.2f", total);
 
-        //задача 5;
+        //задача 5
         System.out.println("задача 5");
-        for (int i = 1904; i <= 2096; i = i + 4) {
-            System.out.println(i + " год является високосным");
+        int charge = 20;
+        int minute = 0;
+        int overheats = 0;
+        while (charge <= 100) {
+            minute++;
+            if (minute >= 10 && minute % 10 == 0) {
+                overheats=overheats+1;
+                minute = minute + 2;
+                System.out.println("Внимание, Перегрев!");
+                continue;
+            }
+            if (overheats > 3) {
+                System.out.println("Зарядка прервана, из-за перегрева");
+                System.out.println("Заряд составил "+ charge+ " процентов");
+                break;
+            }
+            charge = charge + 2;
         }
-
-        //задача 6;
-        System.out.println("задача 6");
-        for (int i = 7; i <= 98; i = i + 7) {
-            System.out.println(i);
-        }
-
-        //задача 7;
-        System.out.println("задача 7");
-        for (int i = 1; i <= 512; i = i * 2) {
-            System.out.println(i);
-        }
-
-        //задача 8;
-        System.out.println("задача 8");
-        int capital = 0;
-        for (int i = 1; i <= 12; i++) {
-            capital = capital + 29000;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + capital + " рублей");
-        }
-
-        //задача 9;
-        System.out.println("задача 9");
-        capital = 0;
-        for (int i = 1; i <= 12; i++) {
-            capital = capital + capital / 100 + 29000;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + capital + " рублей");
-        }
-
-        //задача 10;
-        System.out.println("задача 10");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("2*" + i + "=" + (i * 2));
-        }
+        System.out.println("Время зарядки составило " + minute + " минут");
     }
 }
+
